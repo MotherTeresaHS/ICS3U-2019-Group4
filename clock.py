2f4e98a20aed155aeff7478c582ba3da2e77fb5d
Ubuntu <ubuntu@ip-172-31-30-160.ec2.internal>
2019-12-30 15:20:33 +0000

    added program that displays time and date in a gui

--- clock.py
+++ clock.py
@@ -5,36 +5,35 @@
 # This program displays the time and date
 
 
-from tkinter import * 
+from tkinter import *
 from tkinter.ttk import *
 from time import strftime
 import datetime
-  
-# creating tkinter window 
-root = Tk() 
-root.title('Clock') 
-  
+
+# creating tkinter window
+root = Tk()
+root.title('Clock')
+
+# Styling the label widget
+
+lbl = Label(root, font=('calibri', 40, 'bold'),
+            background='black',
+            foreground='white')
+
+# Placing clock at the centre
+
+lbl.pack(anchor='center')
+
 
 def main():
     # This function displays time and date
 
     string = strftime('%H:%M:%S \n%d-%m-%Y')
-    lbl.config(text = string)
-    lbl.after(1000, main) 
-  
-# Styling the label widget
+    lbl.config(text=string)
+    lbl.after(1000, main)
 
-lbl = Label(root, font = ('calibri', 40, 'bold'), 
-            background = 'black', 
-            foreground = 'white') 
-  
-# Placing clock at the centre 
 
-lbl.pack(anchor = 'center') 
-
- 
 if __name__ == "__main__":
     main()
 
 mainloop()
-
